bcc7e2c2958d9e69c6d147cd77f584c7dc69386b
Chas Fordyce <chasmo123@gmail.com>
2016-05-22 15:52:35 -0700

    Added lots of new things
    .idea files
    Orders file with example order
    Updated WorkOrder
    Added Invoice

--- WorkOrder.java
+++ WorkOrder.java
@@ -7,38 +7,38 @@ public class WorkOrder
 	private int orderNumber;
 	private String customerName;
 	private String makeModel;
-	
+
 	private String dateBegin;
 	private String timeBegin;
-	
+
 	private String dateComplete;
 	private String timeComplete;
-	
+
 	// Constructors
 	public WorkOrder ()
 	{
 		this.orderNumber = 0;
 		this.customerName = null;
 		this.makeModel = null;
-		
+
 		this.dateBegin = null;
 		this.timeBegin = null;
 		this.dateComplete = null;
 		this.timeComplete = null;
 	}
-	
+
 	public WorkOrder (int orderNumber, String customerName, String makeModel, String dateBegin, String timeBegin, String dateComplete, String timeComplete)
 	{
 		this.orderNumber = orderNumber;
 		this.customerName = customerName;
 		this.makeModel = makeModel;
-		
+
 		this.dateBegin = dateBegin;
 		this.timeBegin = timeBegin;
 		this.dateComplete = dateComplete;
 		this.timeComplete = timeComplete;
 	}
-	
+
 	// Setters
 	public void setOrderNumber (int newOrderNumber)
 	{
@@ -52,7 +52,7 @@ public class WorkOrder
 	{
 		this.makeModel = newMakeModel;
 	}
-	
+
 	// Getters
 	public int getOrderNumber ()
 	{
@@ -66,22 +66,22 @@ public class WorkOrder
 	{
 		return this.makeModel;
 	}
-	
+
 	// Methods
 	// Get newest work order number from file and updates the file to the next order number
 	static int getNumberFromFile () throws FileNotFoundException()
 	{
 		File file = new File("ordernumber.txt");
-		
+
 		// Gets the current number
 		Scanner input = new Scanner(file);
 		int number = input.nextInt();
-		
+
 		// Sets the number one higher
 		PrintStream output = new PrintStream(file);
 		output.println((number + 1));
 		output.close();
-		
+
 		return number
 	}
 }
